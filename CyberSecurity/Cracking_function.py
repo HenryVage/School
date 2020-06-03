dd95425c3e271d95c9eacc85d7f7962159c00b54
HenryVage <66363580+HenryVage@users.noreply.github.com>
2020-06-03 14:16:41 +0200

    Update Cracking_function.py

--- CyberSecurity/Cracking_function.py
+++ CyberSecurity/Cracking_function.py
@@ -2,6 +2,7 @@ import sys
 import os
 from string import ascii_uppercase
 
+# Function 2 job is to search the password space and print the password you input in function 1
 def function2(var):
 	for n1 in range(0,10):
 		for n2 in range(0,10):
@@ -21,6 +22,7 @@ def function2(var):
 									print(current)
 									sys.exit("Answer: "+a+b+c+sn1+sn2+sn3+sn4)
 
+# Function 1 takes user input and digests it using openssl with SHA256 hashing algorithm and returns the hash value
 def function1():
 	var = input("please enter password with format uuudddd, like ABC1234: ")
 	output_from_os = os.popen("echo -n"+a+b+c+sn1+sn2+sn3+sn4+"|openssl dgst -hex -sha256")
